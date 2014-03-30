c75d37ca8890e632d284cc074129a3ad44a3624a
hxx <845059331@qq.com>
2014-03-30 20:26:54 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Micropost::Application.routes.draw do
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root              to: 'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help' ,   to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
